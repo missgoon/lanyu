043d9b3b175f6b0c065f30b265f1099f1691bc3c
missgoon <missgoon@163.com>
2016-06-09 18:06:14 +0800

    蓝雨web

---
+++ config/initializers/assets.rb
@@ -0,0 +1,11 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+
+# Add additional assets to the asset load path
+# Rails.application.config.assets.paths << Emoji.images_path
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( 1.jpg 2.jpg 3.jpg 4.jpg 5.jpg 6.jpg 7.jpg 8.jpg 9.jpg 10.jpg 11.jpg 12.jpg 13.jpg layout.css jquery-1.4.1.min.js jquery.slidepanel.setup.js jquery.cycle.min.js jquery.cycle.setup.js)
